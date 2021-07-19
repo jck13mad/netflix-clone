c91e6970737c5fa2497e10105a95b1401b512b03
John Madden <jck13mad@gmail.com>
2021-07-19 11:44:49 -0500

    configured App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -20,6 +20,44 @@ function App() {
       </header>
     </div>
   );
+}
+
+export default App;
+
+
+function App() {
+  return (
+    <div className="app">
+      <div className="nav false">
+        <img 
+          className="nav__logo"
+          src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+          alt=""
+        />
+        <img 
+          className="nav__avatar"
+          src="http://pngimg.com/uploads/netflix/netflix_PNG8.png"
+          alt=""
+        />
+      </div>
+
+      <header className="banner">
+        <div className="banner__contents">
+          <h1 className="banner__title">Ginny &amp; Georgia</h1>
+          <div className="banner__buttons">
+            <button className="banner__button">Play</button>
+            <button className="banner__button">My List</button>
+          </div>
+          <h1 className="banner_description">
+            Angsty and awkward fifteen year old Ginny Miller often feels more
+            mature than her thirty year old mother, the irresistible and dynamic
+            Georgia Miller...
+          </h1>
+        </div>
+        <div className="banner--fadebottom"></div>
+      </header>
+    </div>
+  )
 }
 
 export default App;
