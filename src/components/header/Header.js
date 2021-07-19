da0541f3d619a54c004f5ca29c5ae8e8d9560b3b
John Madden <jck13mad@gmail.com>
2021-07-19 11:55:33 -0500

    Added and set up header.js

---
+++ src/components/header/Header.js
@@ -0,0 +1,21 @@
+function Header() {
+    render (
+        <header className="banner">
+            <div className="banner__contents">
+                <h1 className="banner__title">Ginny &amp; Georgia</h1>
+                <div className="banner__buttons">
+                    <button className="banner__buttons">Play</button>
+                    <button className="banner__buttons">My List</button>
+                </div>
+                <h1 className="banner__description">
+                    Angsty and awkward fifteen year old Ginny Miller often feels more
+                    mature than her thirty year old mother, the irresistible and dynamic
+                    Georgia Miller...
+                </h1>
+            </div>
+            <div className="banner__fadebottom"></div>
+        </header>
+    )
+}
+
+export default Header;
