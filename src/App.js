895a3f29687c8f80aeb13bb1f7a6ae5ee0ff526e
John Madden <jck13mad@gmail.com>
2021-07-19 15:40:43 -0500

    Synced firebase and Row

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Nav from "./components/nav/Nav";
 import Header from "./components/header/Header";
+import Row from "./components/row/Row";
 
 function App() {
   return (
     <div className="app">
       <Nav />
       <Header />
+      <Row title='Netflix Originals' movieType='netflix-originals' />
+      <Row title='Trending Now' movieType='trending-now' />
+      <Row title='Top Rated' movieType='top-rated' />
+      <Row title='Action Movies' movieType='action-movies' />
+      <Row title='Horror Movies' movieType='horror-movies' />
+      <Row title='Romance Movies' movieType='romance-movies' />
+      <Row title='Documentaries Movies' movieType='documentaries-movies' />
     </div>
   )
 }
---
+++ src/components/row/Row.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+import { firebaseDatabase } from "../../firebase/firebase";
+
+function Row(props) {
+  const [movies, setMovies] = useState([]);
+
+  const { title, movieType } = props;
+
+  const leafRoot = 'movies';
+
+  useEffect(() => {
+    fetchMovies(movieType);
+  }, [movieType]);
+
+  const fetchMovies = (movieType) => {
+    const movieRef = firebaseDatabase.ref(`${leafRoot}/${movieType}`);
+    movieRef.on("value", (snapshot) => {
+      const movies = snapshot.val();
+      if (movies && movies.length !== 0) {
+        setMovies(() => movies);
+      }
+    });
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className="row__poster row__posterLarge"
+            src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+            alt={movie.original_name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
